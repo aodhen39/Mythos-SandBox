7ba3af59a9ceaa21bfca254b261b1d9b01a7bef8
aodhen39 <aodhen3@gmail.com>
2021-08-12 18:37:50 +0200

    add : Double jump and Gaps tile correction

--- Just a Flesh Wound/Assets/Scripts/Player/PlayerController.cs
+++ Just a Flesh Wound/Assets/Scripts/Player/PlayerController.cs
@@ -4,29 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Animator animator;
     private Rigidbody2D rb;
+    public Transform groundCheck;
+    //Reference to "Platforms" to know what should be detected as ground
+    public LayerMask whatIsGround;
+   
     private float movementInputDirection;
     private bool isFacingRight = true;
-    public float jumpForce = 16.0f;
+    private bool isRunning;
+    private bool isGrounded;
+    private bool canJump;
 
+    private int amoutJumpsLeft;
+    public int amountOfJump = 1;
+
+    public float jumpForce = 16.0f;
+    public float groundCheckRadius = 1.0f;
     public float movementSpeed = 10.0f;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        amoutJumpsLeft = amountOfJump;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         CheckInput();
-        checkMovementDirection();
+        CheckMovementDirection();
+        UpdateAnimations();
+        CheckIfCanJump();
 
     }
 
     private void FixedUpdate()
     {
         ApplyMovement();
+        CheckSurrounding();
     }
 
     private void CheckInput()
@@ -36,6 +56,12 @@ public class PlayerController : MonoBehaviour
         {
             Jump();
         }
+
+    }
+
+    private void CheckSurrounding()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
     }
 
     private void ApplyMovement()
@@ -45,10 +71,15 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-    }
 
-    private void checkMovementDirection()
+        if (canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amoutJumpsLeft--;
+        }
+    }
+    #region Check Function
+    private void CheckMovementDirection()
     {
         if (isFacingRight && movementInputDirection < 0)
         {
@@ -58,10 +89,48 @@ public class PlayerController : MonoBehaviour
         {
             Flip();
         }
+
+        if(rb.velocity.x > 0.01f || rb.velocity.x < -0.01f)
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+        }
     }
+    private void CheckIfCanJump()
+    {
+        if(isGrounded && rb.velocity.y <= 0)
+        {
+            amoutJumpsLeft = amountOfJump;
+        }
+
+        if(amoutJumpsLeft <= 0)
+        {
+            canJump = false;
+        }
+        else
+        {
+            canJump = true;
+        }
+    }
+#endregion
     private void Flip()
     {
         isFacingRight = !isFacingRight;
         transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void UpdateAnimations()
+    {
+        animator.SetBool("isRunning", isRunning);
+        animator.SetBool("isGrounded", isGrounded);
+        animator.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
