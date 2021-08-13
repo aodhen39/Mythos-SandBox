2ecc9141fb30562efea7d8ae12531fbed5875b30
aodhen39 <aodhen3@gmail.com>
2021-08-13 14:39:37 +0200

    add : slide movement & improve level design

--- Just a Flesh Wound/Assets/Scripts/Player/PlayerController.cs
+++ Just a Flesh Wound/Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
     private Rigidbody2D rb;
     public Transform groundCheck;
+    public Transform wallCheck;
     //Reference to "Platforms" to know what should be detected as ground
     public LayerMask whatIsGround;
    
@@ -14,6 +15,8 @@ public class PlayerController : MonoBehaviour
     private bool isFacingRight = true;
     private bool isRunning;
     private bool isGrounded;
+    private bool isTouchingWall;
+    private bool isWallSliding;
     private bool canJump;
 
     private int amoutJumpsLeft;
@@ -22,7 +25,9 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 16.0f;
     public float groundCheckRadius = 1.0f;
     public float movementSpeed = 10.0f;
-    
+    public float wallCheckDistance;
+    public float wallSlideSpeed;
+    public float movementForceInAir;
     
     // Start is called before the first frame update
     void Start()
@@ -40,6 +45,7 @@ public class PlayerController : MonoBehaviour
         CheckMovementDirection();
         UpdateAnimations();
         CheckIfCanJump();
+        CheckIfWallSliding();
 
     }
 
@@ -62,11 +68,44 @@ public class PlayerController : MonoBehaviour
     private void CheckSurrounding()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
+    }
+
+    private void CheckIfWallSliding()
+    {
+        if(isTouchingWall && !isGrounded && rb.velocity.y < 0)
+        {
+            isWallSliding = true;
+        }
+        else
+        {
+            isWallSliding = false;
+        }
     }
 
     private void ApplyMovement()
     {
-        rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+        rb.velocity = new Vector2( movementSpeed * movementInputDirection, rb.velocity.y );
+
+        if( !isGrounded && !isWallSliding && movementInputDirection != 0 )
+        {
+            Vector2 forceToAdd = new Vector2( movementForceInAir * movementInputDirection, 0 );
+            rb.AddForce( forceToAdd );
+
+            if( Mathf.Abs( rb.velocity.x ) > movementSpeed )
+            {
+                rb.velocity = new Vector2( movementSpeed * movementInputDirection, rb.velocity.y );
+            }
+        }
+
+        if (isWallSliding)
+        {
+            if(rb.velocity.y < -wallSlideSpeed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -wallSlideSpeed);
+            }
+        }
     }
 
     private void Jump()
@@ -127,10 +166,13 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("isRunning", isRunning);
         animator.SetBool("isGrounded", isGrounded);
         animator.SetFloat("yVelocity", rb.velocity.y);
+        animator.SetBool("isWallSliding", isWallSliding);
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+
+        Gizmos.DrawLine(wallCheck.position, new Vector3( wallCheck.position.x + wallCheckDistance, wallCheck.position.y, wallCheck.position.z ));
     }
 }
